5436b4a07d09141a3232ee9c0ad879f413e4c8d9
Anjali Patil <anjali.patil@yourspeechfactory.com>
2024-07-03 12:59:55 +0200

    second commit

--- PaymentValidationAPI/Models/CreditCardInfo.cs
+++ PaymentValidationAPI/Models/CreditCardInfo.cs
@@ -1,9 +1,12 @@
-﻿namespace PaymentValidationAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentValidationAPI.Models
 {
     public class CreditCardInfo
     {
         public string CardOwner { get; set; }
         public string CreditCardNumber { get; set; }
+        [Required]
         public string IssueDate { get; set; }
         public string CVC { get; set; }
 
